9a04b48bc934e136395c3ded5df2bc059eb1f85b
ROHIT GUNDETI <rohitgundeti20017@gmail.com>
2023-07-03 23:49:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react' ;
+import "./App.css";
+import Header from './components/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+const App = () => {
+  return (
+    <>
+    <Header /> 
+   
+     <main className='main'> 
+     
+    <Home/>
+    <About/>
+
+     </main>
+    
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div className="home_social">
+        <a href="https://www.instagram.com/rohitgundeti/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+
+        </a>
+
+        <a href="https://www.linkedin.com/in/rohit-gundeti" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+            
+            </a>
+
+         <a href="https://github.com/" className="home__social-icon" target="_blank">
+         <i class="uil uil-github-alt"></i>
+            
+        </a>
+    </div>
+  )
+}
+
+export default social
